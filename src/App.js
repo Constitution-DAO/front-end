dcae662061b177430c658090336c9dee2df6e7dc
Bulbacode <bulbacode.contact@gmail.com>
2021-11-13 19:29:24 -0600

    Proposal to add AirBnB styleguide
    Adding airbnb styleguide to eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +20,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
